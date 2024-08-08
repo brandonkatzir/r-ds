54a0d26bbd280f338b9adee4ede6edefb34cd4dc
katzir-b <92319301+brandonkatzir@users.noreply.github.com>
2024-08-08 09:39:59 -0400

    adds link and required packages

---
+++ 01-05-data-visualization.R
@@ -0,0 +1,17 @@
+# start w/ 1.5 visualizing relationships 
+# https://r4ds.hadley.nz/data-visualize#visualizing-relationships
+
+#Install packages
+install.packages("tidyverse")
+install.packages("palmerpenguins")
+install.packages("ggthemes")
+install.packages("ggplot2")
+
+#Load packages
+library(tidyverse)
+library(palmerpenguins)
+library(ggthemes)
+library(ggplot2)
+
+#Preview dataset penguins 
+penguins
